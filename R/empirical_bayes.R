116c1b50fe53d51edcaf57a2c37b3e3dc4ca4797
amandaforde <a.forde21@nuigalway.ie>
2021-02-03 12:30:26 +0000

    testing and change to df for EB 03/02

--- R/empirical_bayes.R
+++ R/empirical_bayes.R
@@ -2,7 +2,7 @@
 #'
 #' @param summary_data Data frame containing summary data, three columns: rsid, beta, se
 #'
-#' @return Data frame with summary data together with adjusted estimates but (1) for now
+#' @return Data frame with summary data together with adjusted estimates
 #' @export
 #'
 #'
@@ -14,6 +14,15 @@ empirical_bayes <- function(summary_data)
   bins <- seq(min(z),max(z),length.out=120)
   mids <- (bins[-length(bins)]+bins[-1])/2
   counts <- graphics::hist(z,breaks=bins,plot=F)$counts
+
+  df <- 7
+  AIC_vector <- c(rep(0,28))
+  for (best_df in 3:30){
+    model <- stats::glm(counts ~ splines::ns(mids,df=best_df),stats::poisson,weights=rep(10^-50,length(counts)))
+    minus2loglike <- 10^50*(model$deviance)
+    AIC_vector[best_df-2] <- minus2loglike + 2*(best_df-2)
+  }
+  df <- 2 + which.min(AIC_vector)
 
   f <- stats::glm(counts ~ splines::ns(mids,df=7),stats::poisson,weight=rep(10^-50,length(counts)))$fit
   f[f==0] <- min(f[f>0])
@@ -34,7 +43,7 @@ empirical_bayes <- function(summary_data)
   beta_EB <- z_hat*summary_data$se
   summary_data <- cbind(summary_data,beta_EB)
 
-  return(1)
+  return(summary_data)
 
 }
 
--- tests/testthat/test-empirical_bayes.R
+++ tests/testthat/test-empirical_bayes.R
@@ -7,11 +7,19 @@ context("empirical bayes")
 test_that("testing example",
 
           {
+            N <- 10^5
+            N_poly_bg <- 1000
+            beta_true <- c(rnorm(N-N_poly_bg,sd=.1),rnorm(N_poly_bg,sd=1))
+            MAF <- runif(N,min=0.02,max=0.5)
+            SE <- 1/sqrt(2*MAF*(1-MAF))
+            beta_hat <- rnorm(length(beta_true),mean=beta_true,sd=SE)
 
-            summary_data <- data.frame(rsID = c(1,2),beta = c(0.49,0.645), se =c(0.25,0.25))
+            summary_data <- data.frame(rsid = seq(1,N), beta = beta_hat, se = SE)
 
             out <- empirical_bayes(summary_data)
 
-            expect_true(out == 1)
+            test <- sum(abs(round(out$beta,6)) >= abs(round(out$beta_EB,6))) == length(out$beta)
+
+            expect_true(identical(test,TRUE) == 1)
 
           })
