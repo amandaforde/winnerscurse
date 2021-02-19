16ffcce578787c0f52b8da0347da9069e904448d
amandaforde <a.forde21@nuigalway.ie>
2021-02-19 14:52:59 +0000

    BR_ss re-add 19/02

--- R/BR_ss.R
+++ R/BR_ss.R
@@ -7,7 +7,7 @@
 
 BR_ss <- function(summary_data){
 
-  summary_data <- dplyr::arrange(summary_data, dplyr::desc(abs(summary_data$beta/summary_data$se)))
+  summary_data <- dplyr::arrange(summary_data, dplyr::desc((summary_data$beta/summary_data$se)))
 
   N <- nrow(summary_data)
   beta_boot <- matrix(stats::rnorm(100*N, mean = rep(summary_data$beta,100), sd = rep(summary_data$se,100)), nrow=N, ncol=100, byrow=FALSE)
@@ -29,6 +29,8 @@ BR_ss <- function(summary_data){
   beta_BR_ss[sign(beta_BR_ss) != sign(summary_data$beta)] <- 0
 
   summary_data <- cbind(summary_data, beta_BR_ss)
+
+  summary_data <- dplyr::arrange(summary_data,dplyr::desc(abs(summary_data$beta/summary_data$se)))
 
   return(summary_data)
 }
