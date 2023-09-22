5deada2fca8ef5876ba009bf444586877c31d3f5
GenDataSci025 <GenDataSci025@NUIG-7CN1P53.uds.nuigalway.ie>
2023-09-22 08:47:17 +0100

    improve coverage 22/09

--- tests/testthat/test-BR_ss.R
+++ tests/testthat/test-BR_ss.R
@@ -10,7 +10,6 @@ test_that("testing if BR_ss is giving appropriate output",
             n_snps <- 10^6
             effect_snps <- 10000
             n_samples <- 30000
-
             maf <- runif(n_snps,0.01,0.5)
             se <- 1/sqrt(2*n_samples*maf*(1-maf))
 
@@ -27,7 +26,18 @@ test_that("testing if BR_ss is giving appropriate output",
             out_sig <- out[2*(1-pnorm(abs(out$beta/out$se))) < 5e-8,]
 
             test <- sum(abs(round(out_sig$beta,6)) >= abs(round(out_sig$beta_BR_ss,6))) >= length(out_sig$beta)*0.7
-
             expect_true(identical(test,TRUE) == 1)
+
+
+            maf <- c(0.22,0.42,0.42,0.08,0.39,0.29,0.47,0.49,0.14,0.13)
+            se <- 1/sqrt(2*30000*maf*(1-maf))
+            beta <- c(0.08,0.59,0.1,0.1,0.3,-0.1,-0.95,0.04,-0.29,0.05)
+            summary_stats <- data.frame(rsid=seq(1,10),beta=beta,se=se)
+
+            out1 <- BR_ss(summary_stats,seed_opt=TRUE)
+            out2 <- BR_ss(summary_stats,seed_opt=TRUE)
+
+            test <- as.numeric(sum(out1 == out2))
+            expect_true(identical(test,40) == 1)
 
           })
